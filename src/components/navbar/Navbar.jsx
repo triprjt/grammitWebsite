c1a7869994179b78d738517d14504a04eb1bc916
triprjt <triprjt@gmail.com>
2022-07-19 00:17:33 +0530

    trial for mobile

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -26,8 +26,8 @@ const Navbar = () => {
         {toggleMenu && (
           <div className = "gpt3__navbar-menu_container scale-up-center">
             <div className="gpt3__navbar-menu_container-links">
-              <p><a href = "#wpaper">Whitepaper</a></p>
-              <p><a href = "#comm">Join the community</a></p>
+              <p><a onClick={WhitepaperOpen}>Whitepaper</a></p>
+              <p><a href = "https://discord.gg/uJbmBWjJup">Join the community</a></p>
             </div>
           </div>
         )}
